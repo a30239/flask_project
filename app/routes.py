f30e6889c1834b11afb43a92192a9d5148f4e31c
maoym <1012152056@qq..com>
2019-12-16 21:57:34 +0800

    修复下拉菜单

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,21 @@ from app import app
 #从app模块中导入创建的实例 （__init__.py中创建）
 from flask import render_template
 #导入渲染html模板
+import mysql.connector
+
+#数据库连接
+config ={'host':'122.51.219.231',#默认127.0.0.1
+        'user':'conuser',
+        'password':'TXYMySQL@Connect2019',
+        'port':3306 ,#默认即为3306
+        'database':'g96-data',
+        'charset':'utf8'#默认即为utf8
+        }
+try:
+  cnn = mysql.connector.connect(**config)
+  print('connet success')
+except mysql.connector.Error as e:
+  print('connect fails!{}'.format(e))#输出连接错误信息
 
 @app.route('/')#建立路由
 @app.route('/index') #通过路由可以执行其覆盖的方法，可以多个路由指向同一个方法
@@ -10,4 +25,5 @@ def index():
     return render_template('index.html',title = '你好',user = {'username':'aa'})
 @app.route('/test')
 def test():
+    
     return render_template('test.html')
